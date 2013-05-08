a1ef34fa7c0c5bdea6133d5acfbcbd7b1da3bea5
Jared Hoberock <jaredhoberock@gmail.com>
2013-05-08 14:13:30 -0700

    Return iterators from copy functions like STL.

--- copy.h
+++ copy.h
@@ -7,27 +7,32 @@ namespace block
 
 template<typename Context, typename Iterator1, typename Size, typename Iterator2>
 __device__
-void async_copy_n(Context &ctx, Iterator1 first, Size n, Iterator2 result)
+Iterator2 async_copy_n(Context &ctx, Iterator1 first, Size n, Iterator2 result)
 {
   for(Size i = ctx.thread_index(); i < n; i += ctx.block_dimension())
   {
     result[i] = first[i];
   }
+
+  return result + n;
 }
 
 
 template<typename Context, typename Iterator1, typename Size, typename Iterator2>
 __device__
-void copy_n(Context &ctx, Iterator1 first, Size n, Iterator2 result)
+Iterator2 copy_n(Context &ctx, Iterator1 first, Size n, Iterator2 result)
 {
-  async_copy_n(ctx, first, n, result);
+  result = async_copy_n(ctx, first, n, result);
+
   ctx.barrier();
+
+  return result;
 }
 
 
 template<unsigned int work_per_thread, typename Context, typename Iterator1, typename Size, typename Iterator2>
 __device__
-void async_copy_n_global_to_shared(Context &ctx, Iterator1 first, Size n, Iterator2 result)
+Iterator2 async_copy_n_global_to_shared(Context &ctx, Iterator1 first, Size n, Iterator2 result)
 {
   typedef typename thrust::iterator_value<Iterator1>::type value_type;
 
@@ -73,15 +78,20 @@ void async_copy_n_global_to_shared(Context &ctx, Iterator1 first, Size n, Iterat
       if(idx < n) result[idx] = reg[i];
     }
   }
+
+  return result + n;
 }
 
 
 template<unsigned int work_per_thread, typename Context, typename Iterator1, typename Size, typename Iterator2>
 __device__
-void copy_n_global_to_shared(Context &ctx, Iterator1 first, Size n, Iterator2 result)
+Iterator2 copy_n_global_to_shared(Context &ctx, Iterator1 first, Size n, Iterator2 result)
 {
-  async_copy_n_global_to_shared<work_per_thread>(ctx, first, n, result);
+  result = async_copy_n_global_to_shared<work_per_thread>(ctx, first, n, result);
+
   ctx.barrier();
+
+  return result;
 }
 
 
